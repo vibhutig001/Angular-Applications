11aa80776bd04724d7eb7c229a83095b455cf37e
Vibhuti Gudaghe <99797163+vibhutig001@users.noreply.github.com>
2023-12-28 17:44:27 +0530

    Add files via upload

---
+++ Assignment5(3)/app/first-comp/first-comp.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondCompComponent } from '../second-comp/second-comp.component';
+@Component({
+  selector: 'app-first-comp',
+  standalone: true,
+  imports: [SecondCompComponent],
+  templateUrl: './first-comp.component.html',
+  styleUrl: './first-comp.component.css'
+})
+export class FirstCompComponent {
+
+}
