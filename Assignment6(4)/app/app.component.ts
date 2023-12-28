2e483764ddfe4c4c875f53cba73d17ae3a7e2845
Vibhuti Gudaghe <99797163+vibhutig001@users.noreply.github.com>
2023-12-28 17:46:50 +0530

    Add files via upload

---
+++ Assignment6(4)/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+import { NewCompComponent } from './new-comp/new-comp.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,NewCompComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'n3';
+}
