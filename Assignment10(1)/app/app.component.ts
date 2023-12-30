f848ba1b66fd40ab505ba191d6b0f73b6e484d7b
Vibhuti Gudaghe <99797163+vibhutig001@users.noreply.github.com>
2023-12-30 14:21:58 +0530

    Add files via upload

---
+++ Assignment10(1)/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+import { CompSuccessDirective } from './comp-success.directive';
+import { CompFailureDirective } from './comp-failure.directive';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,CompSuccessDirective,CompFailureDirective],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  
+}
